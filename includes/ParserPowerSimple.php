0026afdcd13b39a22d8bb99253038ba8b79a3d67
rheingoldriver <18037011+RheingoldRiver@users.noreply.github.com>
2024-02-28 23:33:24 +0000

    fix

--- includes/ParserPowerSimple.php
+++ includes/ParserPowerSimple.php
@@ -14,6 +14,8 @@ namespace ParserPower;
 use MediaWiki\MediaWikiServices;
 use PPFrame;
 use Title;
+use Parser;
+use PPNode_Hash_Array;
 
 class ParserPowerSimple {
 	/**
@@ -27,22 +29,22 @@ class ParserPowerSimple {
 		$parser->setFunctionHook(
 			'trim',
 			'ParserPower\\ParserPowerSimple::trimRender',
-			SFH_OBJECT_ARGS
+			Parser::SFH_OBJECT_ARGS
 		);
 		$parser->setFunctionHook(
 			'uesc',
 			'ParserPower\\ParserPowerSimple::uescRender',
-			SFH_OBJECT_ARGS
+			Parser::SFH_OBJECT_ARGS
 		);
 		$parser->setFunctionHook(
 			'uescnowiki',
 			'ParserPower\\ParserPowerSimple::uescnowikiRender',
-			SFH_OBJECT_ARGS
+			Parser::SFH_OBJECT_ARGS
 		);
 		$parser->setFunctionHook(
 			'trimuesc',
 			'ParserPower\\ParserPowerSimple::trimuescRender',
-			SFH_OBJECT_ARGS
+			Parser::SFH_OBJECT_ARGS
 		);
 		$parser->setHook(
 			'linkpage',
@@ -65,38 +67,50 @@ class ParserPowerSimple {
 		$parser->setFunctionHook(
 			'ueif',
 			'ParserPower\\ParserPowerSimple::ueifRender',
-			SFH_OBJECT_ARGS
+			Parser::SFH_OBJECT_ARGS
 		);
 		$parser->setFunctionHook(
 			'or',
 			'ParserPower\\ParserPowerSimple::orRender',
-			SFH_OBJECT_ARGS
+			Parser::SFH_OBJECT_ARGS
 		);
 		$parser->setFunctionHook(
 			'ueifeq',
 			'ParserPower\\ParserPowerSimple::ueifeqRender',
-			SFH_OBJECT_ARGS
+			Parser::SFH_OBJECT_ARGS
 		);
 		$parser->setFunctionHook(
 			'token',
 			'ParserPower\\ParserPowerSimple::tokenRender',
-			SFH_OBJECT_ARGS
+			Parser::SFH_OBJECT_ARGS
 		);
 		$parser->setFunctionHook(
 			'tokenif',
 			'ParserPower\\ParserPowerSimple::tokenifRender',
-			SFH_OBJECT_ARGS
+			Parser::SFH_OBJECT_ARGS
 		);
 		$parser->setFunctionHook(
 			'ueswitch',
 			'ParserPower\\ParserPowerSimple::ueswitchRender',
-			SFH_OBJECT_ARGS
+			Parser::SFH_OBJECT_ARGS
 		);
 		$parser->setFunctionHook(
 			'follow',
 			'ParserPower\\ParserPowerSimple::followRender',
-			SFH_OBJECT_ARGS
+			Parser::SFH_OBJECT_ARGS
 		);
+		
+		if ( defined( 'PF_VERSION' ) ) {
+			// Do not load if Page Forms is installed.
+			return;
+		}
+		
+		$parser->setFunctionHook( 'arraymap', 'ParserPower\\ParserPowerSimple::arraymapRender',
+			Parser::SFH_OBJECT_ARGS );
+
+		$parser->setFunctionHook( 'arraymaptemplate', 'ParserPower\\ParserPowerSimple::arraymaptemplateRender',
+			Parser::SFH_OBJECT_ARGS );
+		
 	}
 
 	/**
@@ -427,4 +441,98 @@ class ParserPowerSimple {
 
 		return [$output, 'noparse' => false];
 	}
+	
+	public static function arraymapRender($parser, $frame, $args) {
+		// Set variables.
+		$value = isset( $args[0] ) ? trim( $frame->expand( $args[0] ) ) : '';
+		$delimiter = isset( $args[1] ) ? trim( $frame->expand( $args[1] ) ) : ',';
+		$var = isset( $args[2] ) ? trim( $frame->expand( $args[2], PPFrame::NO_ARGS | PPFrame::NO_TEMPLATES ) ) : 'x';
+		$formula = isset( $args[3] ) ? $args[3] : 'x';
+		$new_delimiter = isset( $args[4] ) ? trim( $frame->expand( $args[4] ) ) : ', ';
+		$conjunction = isset( $args[5] ) ? trim( $frame->expand( $args[5] ) ) : $new_delimiter;
+		// Unstrip some.
+		$delimiter = $parser->getStripState()->unstripNoWiki( $delimiter );
+		// Let '\n' represent newlines, and '\s' represent spaces.
+		$delimiter = str_replace( [ '\n', '\s' ], [ "\n", ' ' ], $delimiter );
+		$new_delimiter = str_replace( [ '\n', '\s' ], [ "\n", ' ' ], $new_delimiter );
+		$conjunction = str_replace( [ '\n', '\s' ], [ "\n", ' ' ], $conjunction );
+
+		if ( $delimiter == '' ) {
+			$values_array = preg_split( '/(.)/u', $value, -1, PREG_SPLIT_NO_EMPTY | PREG_SPLIT_DELIM_CAPTURE );
+		} else {
+			$values_array = explode( $delimiter, $value );
+		}
+
+		$results_array = [];
+		// Add results to the results array only if the old value was
+		// non-null, and the new, mapped value is non-null as well.
+		foreach ( $values_array as $old_value ) {
+			$old_value = trim( $old_value );
+			if ( $old_value == '' ) {
+				continue;
+			}
+			$result_value = $frame->expand( $formula, PPFrame::NO_ARGS | PPFrame::NO_TEMPLATES );
+			$result_value = str_replace( $var, $old_value, $result_value );
+			$result_value = $parser->preprocessToDom( $result_value, $frame->isTemplate() ? Parser::PTD_FOR_INCLUSION : 0 );
+			$result_value = trim( $frame->expand( $result_value ) );
+			if ( $result_value == '' ) {
+				continue;
+			}
+			$results_array[] = $result_value;
+		}
+		if ( $conjunction != $new_delimiter ) {
+			$conjunction = " " . trim( $conjunction ) . " ";
+		}
+
+		$result_text = "";
+		$num_values = count( $results_array );
+		for ( $i = 0; $i < $num_values; $i++ ) {
+			if ( $i == 0 ) {
+				$result_text .= $results_array[$i];
+			} elseif ( $i == $num_values - 1 ) {
+				$result_text .= $conjunction . $results_array[$i];
+			} else {
+				$result_text .= $new_delimiter . $results_array[$i];
+			}
+		}
+		return $result_text;
+	}
+
+	public static function arraymaptemplateRender($parser, $frame, $args) {
+		// Set variables.
+		$value = isset( $args[0] ) ? trim( $frame->expand( $args[0] ) ) : '';
+		$template = isset( $args[1] ) ? trim( $frame->expand( $args[1] ) ) : '';
+		$delimiter = isset( $args[2] ) ? trim( $frame->expand( $args[2] ) ) : ',';
+		$new_delimiter = isset( $args[3] ) ? trim( $frame->expand( $args[3] ) ) : ', ';
+		// Unstrip some.
+		$delimiter = $parser->getStripState()->unstripNoWiki( $delimiter );
+		// let '\n' represent newlines
+		$delimiter = str_replace( '\n', "\n", $delimiter );
+		$new_delimiter = str_replace( '\n', "\n", $new_delimiter );
+
+		if ( $delimiter == '' ) {
+			$values_array = preg_split( '/(.)/u', $value, -1, PREG_SPLIT_NO_EMPTY | PREG_SPLIT_DELIM_CAPTURE );
+		} else {
+			$values_array = explode( $delimiter, $value );
+		}
+
+		$results_array = [];
+		foreach ( $values_array as $old_value ) {
+			$old_value = trim( $old_value );
+			if ( $old_value == '' ) {
+				continue;
+			}
+			$bracketed_value = $frame->virtualBracketedImplode( '{{', '|', '}}',
+				$template, '1=' . $old_value );
+			// Special handling if preprocessor class is set to
+			// 'Preprocessor_Hash'.
+			if ( $bracketed_value instanceof PPNode_Hash_Array ) {
+				$bracketed_value = $bracketed_value->value;
+			}
+			$results_array[] = $parser->replaceVariables(
+				implode( '', $bracketed_value ), $frame );
+		}
+		return implode( $new_delimiter, $results_array );
+	}
+	
 }
