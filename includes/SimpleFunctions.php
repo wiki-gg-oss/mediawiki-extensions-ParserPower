63dcb190bbedca3e402b85273dcf5b4869cf9e60
Adrien LESÉNÉCHAL <lesenechala@laposte.net>
2024-12-16 18:24:47 +0100

    `#follow`: override redirect fragment with original one

--- includes/SimpleFunctions.php
+++ includes/SimpleFunctions.php
@@ -391,6 +391,12 @@ final class SimpleFunctions {
 			return $text;
 		}
 
+		// Replace redirect fragment with the one from the initial text. We need to check whether there is
+		// a # with no fragment after it, since it removes the redirect fragment if there is one.
+		if ( strpos( $text, '#' ) ) {
+			$target->setFragment( '#' . $title->getFragment() );
+		}
+
 		return $target->getFullText();
 	}
 
