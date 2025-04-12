ce4b1556fadeb8b84cb6d2629dc624369b594b82
Adrien LESÉNÉCHAL <lesenechala@laposte.net>
2025-04-12 15:49:26 +0200

    ListFunctions: Move list value modification logic to separate classes Add a WikitextOperation interface for operations that we can apply() to a list value, with 3 implementations: - ListInclusionOperation for finding the list value in a known value list, - PatternOperation for replacing tokens with the list value in a pattern, - TemplateOperation for transcluding a template with the list value as parameters.

---
+++ src/Operation/ListInclusionOperation.php
@@ -0,0 +1,66 @@
+<?php
+
+/** @license GPL-2.0-or-later */
+
+namespace MediaWiki\Extension\ParserPower\Operation;
+
+use MediaWiki\Extension\ParserPower\ParserPower;
+use MediaWiki\Parser\Parser;
+use MediaWiki\Parser\PPFrame;
+
+/**
+ * List value operation that checks whether the list value is in a list.
+ */
+final class ListInclusionOperation implements WikitextOperation {
+
+	/**
+	 * Array with values to look for as keys.
+	 */
+	private array $valueSet;
+
+	/**
+	 * @param array $values Values to look for.
+	 * @param string $resultWhenIn Variable-free wikitext to return when the list value is included.
+	 * @param string $resultWhenOut Variable-free wikitext to return when the list value is not included.
+	 * @param bool $caseSensitive True if case sentitive, false otherwise.
+	 */
+	public function __construct(
+		array $values,
+		private string $resultWhenIn,
+		private string $resultWhenOut = '',
+		private bool $caseSensitive = true
+	) {
+		if ( $caseSensitive ) {
+			foreach ( $values as $value ) {
+				$this->valueSet[$value] = true;
+			}
+		} else {
+			foreach ( $values as $value ) {
+				$this->valueSet[strtolower( $value )] = true;
+			}
+		}
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function apply( array $fields, ?int $index = null ): string {
+		$value = $fields[0];
+		if ( !$this->caseSensitive ) {
+			$value = strtolower( $value );
+		}
+
+		if ( isset( $this->valueSet[$value] ) ) {
+			return $this->resultWhenIn;
+		} else {
+			return $this->resultWhenOut;
+		}
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function getFieldLimit(): ?int {
+		return 1;
+	}
+}
