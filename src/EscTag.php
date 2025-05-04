9d1eb3e276e82ad3a78b8e121fb2466f738363aa
Adrien LESÉNÉCHAL <lesenechala@laposte.net>
2025-05-04 21:07:37 +0200

    SimpleFunctions: Split <esc> tags

--- src/EscTag.php
+++ src/EscTag.php
@@ -7,7 +7,23 @@ namespace MediaWiki\Extension\ParserPower;
 use MediaWiki\Parser\Parser;
 use MediaWiki\Parser\PPFrame;
 
-final class SimpleFunctions {
+/**
+ * Tag for escaping a value (<esc>, <esc1>, <esc2>, ...).
+ */
+final class EscTag {
+
+	/**
+	 * Gets the list of all available tag names.
+	 *
+	 * @return array The list of parser tag names.
+	 */
+	public function getNames(): array {
+		$names = [ 'esc' ];
+		for ( $index = 1; $index < 10; $index++ ) {
+			$names[] = 'esc' . $index;
+		}
+		return $names;
+	}
 
 	/**
 	 * This function escapes all appropriate characters in the given text and returns the result.
@@ -18,7 +34,7 @@ final class SimpleFunctions {
 	 * @param PPFrame $frame The parser frame object.
 	 * @return string The function output.
 	 */
-	public function escTagRender( ?string $text, array $attribs, Parser $parser, PPFrame $frame ): array {
+	public function render( ?string $text, array $attribs, Parser $parser, PPFrame $frame ): array {
 		return [ ParserPower::escape( $text ), 'markerType' => 'none' ];
 	}
 }
