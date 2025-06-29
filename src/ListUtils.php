bfe831b2f1dfe379377a42598cc43f2f87a947a5
Adrien LESÉNÉCHAL <lesenechala@laposte.net>
2025-06-29 20:38:41 +0200

    Inline `ListUtils::PARAM_OPTIONS`

--- src/ListUtils.php
+++ src/ListUtils.php
@@ -49,51 +49,6 @@ final class ListUtils {
 	public const SORTMODE_POST = 2;
 	public const SORTMODE_COMPAT = 4;
 
-	public const PARAM_OPTIONS = [
-		'counttoken' => [ 'unescape' => true ],
-		'csoption' => [],
-		'default' => [ 'unescape' => true ],
-		'duplicates' => [],
-		'fieldsep' => [ 'unescape' => true ],
-		'keep' => [],
-		'keepcs' => [],
-		'keepsep' => [ 'default' => ',' ],
-		'index' => [ 'unescape' => true ],
-		'indexoptions' => [],
-		'indextoken' => [ 'unescape' => true ],
-		'insep' => [ 'unescape' => true, 'default' => ',' ],
-		'insep1' => [ 'unescape' => true, 'default' => ',' ],
-		'insep2' => [ 'unescape' => true, 'default' => ',' ],
-		'intro' => [ 'unescape' => true ],
-		'length' => [ 'unescape' => true ],
-		'list' => [],
-		'list1' => [],
-		'list2' => [],
-		'outro' => [ 'unescape' => true ],
-		'outsep' => [ 'unescape' => true, 'default' => ', ' ],
-		'matchpattern' => [],
-		'matchtemplate' => [],
-		'mergepattern' => [],
-		'mergetemplate' => [],
-		'outconj' => [ 'unescape' => true ],
-		'pattern' => [],
-		'remove' => [],
-		'removecs' => [],
-		'removesep' => [ 'default' => ',' ],
-		'removecs' => [],
-		'sortmode' => [],
-		'sortoptions' => [],
-		'subsort' => [],
-		'subsortoptions' => [],
-		'template' => [],
-		'token' => [ 'unescape' => true ],
-		'token1' => [ 'unescape' => true ],
-		'token2' => [ 'unescape' => true ],
-		'tokensep' => [ 'unescape' => true, 'default' => ',' ],
-		'uniquecs' => [],
-		'value' => [ 'unescape' => true ]
-	];
-
 	/**
 	 * This function converts a string containing a boolean keyword into a boolean.
 	 *
