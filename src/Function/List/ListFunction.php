233530ad7b6dcebe1aec2971f5dc5a0fd51cf753
Adrien LESÉNÉCHAL <lesenechala@laposte.net>
2025-06-29 20:38:41 +0200

    Inline `ListUtils::PARAM_OPTIONS`

--- src/Function/List/ListFunction.php
+++ src/Function/List/ListFunction.php
@@ -15,6 +15,49 @@ abstract class ListFunction extends ParserFunctionBase {
 	 * @inheritDoc
 	 */
 	public function getParamSpec(): array {
-		return ListUtils::PARAM_OPTIONS;
+		return [
+			'counttoken' => [ 'unescape' => true ],
+			'csoption' => [],
+			'default' => [ 'unescape' => true ],
+			'duplicates' => [],
+			'fieldsep' => [ 'unescape' => true ],
+			'keep' => [],
+			'keepcs' => [],
+			'keepsep' => [ 'default' => ',' ],
+			'index' => [ 'unescape' => true ],
+			'indexoptions' => [],
+			'indextoken' => [ 'unescape' => true ],
+			'insep' => [ 'unescape' => true, 'default' => ',' ],
+			'insep1' => [ 'unescape' => true, 'default' => ',' ],
+			'insep2' => [ 'unescape' => true, 'default' => ',' ],
+			'intro' => [ 'unescape' => true ],
+			'length' => [ 'unescape' => true ],
+			'list' => [],
+			'list1' => [],
+			'list2' => [],
+			'outro' => [ 'unescape' => true ],
+			'outsep' => [ 'unescape' => true, 'default' => ', ' ],
+			'matchpattern' => [],
+			'matchtemplate' => [],
+			'mergepattern' => [],
+			'mergetemplate' => [],
+			'outconj' => [ 'unescape' => true ],
+			'pattern' => [],
+			'remove' => [],
+			'removecs' => [],
+			'removesep' => [ 'default' => ',' ],
+			'removecs' => [],
+			'sortmode' => [],
+			'sortoptions' => [],
+			'subsort' => [],
+			'subsortoptions' => [],
+			'template' => [],
+			'token' => [ 'unescape' => true ],
+			'token1' => [ 'unescape' => true ],
+			'token2' => [ 'unescape' => true ],
+			'tokensep' => [ 'unescape' => true, 'default' => ',' ],
+			'uniquecs' => [],
+			'value' => [ 'unescape' => true ]
+		];
 	}
 }
