a16c2f01466e012a636e5b371f472f87d1984181
Adrien LESÉNÉCHAL <lesenechala@laposte.net>
2025-08-04 19:57:35 +0200

    `#lstfltr`/`#lstrm`: Reuse `execute()` from `#listfilter` However, both of these functions do not work exactly the same way as #listfilter.
    To prevent breakage, we make some parameter default values different from #listfilter:
    - #lstrm has no {{{removesep}}} by default, while #listfilter make it default to a comma.
    - #lstfltr removes all values if {{{keep}}} is empty, while #listtfilter keeps all values, which we can circumvent by providing a default {{{pattern}}}.

--- src/Function/List/LstFltrFunction.php
+++ src/Function/List/LstFltrFunction.php
@@ -34,43 +34,21 @@ final class LstFltrFunction extends ListFilterFunction {
 	 * @inheritDoc
 	 */
 	public function getParamSpec(): array {
-		return [
+		$paramSpec = [
 			...parent::getParamSpec(),
 			0 => 'keep',
 			1 => 'keepsep',
 			2 => 'list',
 			3 => 'insep',
 			4 => 'outsep',
-			5 => 'csoption'
+			5 => [
+				'alias' => 'keepcs',
+				'formatter' => $this->getCSFormatter()
+			]
 		];
-	}
 
-	/**
-	 * @inheritDoc
-	 */
-	public function execute( Parser $parser, PPFrame $frame, Parameters $params ): string {
-		$inList = $params->get( 'list' );
-		$inSep = $inList !== '' ? $params->get( 'insep' ) : '';
-		$inSep = $parser->getStripState()->unstripNoWiki( $inSep );
-		$inValues = ListUtils::explode( $inSep, $inList );
+		$paramSpec['pattern']['default'] = 'remove';
 
-		if ( empty( $inValues ) ) {
-			return '';
-		}
-
-		$values = $params->get( 'keep' );
-		$valueSep = $params->get( 'keepsep' );
-		$csOption = $params->get( 'csoption' );
-
-		if ( $valueSep !== '' ) {
-			$values = ListUtils::explode( $valueSep, $values );
-		} else {
-			$values = [ ParserPower::unescape( $values ) ];
-		}
-
-		$operation = new ListInclusionOperation( $values, '', 'remove', $csOption );
-		$outValues = $this->filterList( $operation, $inValues );
-
-		return ParserPower::evaluateUnescaped( $parser, $frame, $this->implodeOutList( $params, $outValues ) );
+		return $paramSpec;
 	}
 }
