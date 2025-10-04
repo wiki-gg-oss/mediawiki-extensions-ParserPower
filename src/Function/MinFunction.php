978556c406dc1ca123d2a0196b2a521026db1fb9
alex4401 <alex@wiki.gg>
2025-10-04 13:38:09 +0200

    `#min`, `#max`: Improve the error message for non-numeric parameters
    Issue: WIKIDEV-1038

--- src/Function/MinFunction.php
+++ src/Function/MinFunction.php
@@ -47,7 +47,7 @@ final class MinFunction extends ParserFunctionBase {
 
 			// Throw an error if the parameter is non-numeric
 			if ( !is_numeric( $inValue ) ) {
-				return ParserPower::errorMessage( 'min', 'parserpower-error-invalid-number', $inValue );
+				return ParserPower::errorMessage( 'min', 'invalid-number', $inValue );
 			}
 
 			$inValue = floatval( $inValue );
